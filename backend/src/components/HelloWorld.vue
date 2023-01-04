cbd8afb50dcbff9b9f5d2f51c45eb9029f71db69
Yarosalav <bondarchuck.yaroslaw@gmail.com>
2023-01-04 21:21:41 +0200

    init

---
+++ backend/src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref, computed } from 'vue'
+const test = computed(() => store.state.test)
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <p> {{ test }} </p>
+  </div>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit 
+    </p>
+  </div>
+
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: rgb(250, 210, 210);
+}
+</style>
